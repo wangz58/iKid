0e79056c6a816dc0907dcc3b2c0ed30ad6ef07f1
wangz58 <wangz58@uw.edu>
2016-04-29 23:01:35 -0700

    finished all

---
+++ iKid/iKid/BetterSwitchViewController.swift
@@ -0,0 +1,80 @@
+//
+//  BetterSwitchViewController.swift
+//  iKid
+//
+//  Created by apple on 4/29/16.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+import UIKit
+
+class BetterSwitchViewController: UIViewController {
+    
+    private var firstViewController : BetterFirstViewController!
+    private var secondViewController : BetterSecondViewController!
+    
+    private func firstBuilder() {
+        if firstViewController == nil { firstViewController =
+            storyboard?.instantiateViewControllerWithIdentifier("BetterFirst")
+            as! BetterFirstViewController
+        }
+    }
+    private func secondBuilder() {
+        if secondViewController == nil { secondViewController =
+            storyboard?.instantiateViewControllerWithIdentifier("BetterSecond")
+            as! BetterSecondViewController
+        }
+    }
+    
+    @IBAction func switchViews(sender: UIBarButtonItem) {
+        secondBuilder()
+        firstBuilder()
+        if firstViewController != nil &&
+            firstViewController?.view.superview != nil {
+            UIView.setAnimationTransition(.FlipFromRight, forView: view, cache: true)
+            secondViewController.view.frame = view.frame
+            switchViewController(firstViewController, to: secondViewController)
+        } else {
+            UIView.setAnimationTransition(.FlipFromLeft, forView: view, cache: true)
+            firstViewController.view.frame = view.frame
+            switchViewController(secondViewController, to: firstViewController)
+        }
+    }
+    
+    private func switchViewController(from: UIViewController?, to:
+        UIViewController?) {
+        if from != nil {
+            from!.willMoveToParentViewController(nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParentViewController()
+        }
+        if to != nil {
+            self.addChildViewController(to!)
+            self.view.insertSubview(to!.view, atIndex: 0)
+            to!.didMoveToParentViewController(self)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iKid/iKid/GoodSwitchViewController.swift
@@ -0,0 +1,81 @@
+//
+//  GoodSwitchViewController.swift
+//  iKid
+//
+//  Created by apple on 4/29/16.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+import UIKit
+
+class GoodSwitchViewController: UIViewController {
+    
+    private var firstViewController : GoodFirstViewController!
+    private var secondViewController : GoodSecondViewController!
+    
+    private func firstBuilder() {
+        if firstViewController == nil { firstViewController =
+            storyboard?.instantiateViewControllerWithIdentifier("GoodFirst")
+            as! GoodFirstViewController
+        }
+    }
+    private func secondBuilder() {
+        if secondViewController == nil { secondViewController =
+            storyboard?.instantiateViewControllerWithIdentifier("GoodSecond")
+            as! GoodSecondViewController
+        }
+    }
+    
+    @IBAction func switchViews(sender: UIBarButtonItem) {
+        secondBuilder()
+        firstBuilder()
+        
+        if firstViewController != nil &&
+            firstViewController?.view.superview != nil {
+            UIView.setAnimationTransition(.FlipFromRight, forView: view, cache: true)
+            secondViewController.view.frame = view.frame
+            switchViewController(firstViewController, to: secondViewController)
+        } else {
+            UIView.setAnimationTransition(.FlipFromLeft, forView: view, cache: true)
+            firstViewController.view.frame = view.frame
+            switchViewController(secondViewController, to: firstViewController)
+        }
+    }
+    
+    private func switchViewController(from: UIViewController?, to:
+        UIViewController?) {
+        if from != nil {
+            from!.willMoveToParentViewController(nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParentViewController()
+        }
+        if to != nil {
+            self.addChildViewController(to!)
+            self.view.insertSubview(to!.view, atIndex: 0)
+            to!.didMoveToParentViewController(self)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
